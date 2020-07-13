f6a7e40b4d9933fdce852e81817d4ec107b4e01c
Sergey Bokatuk <s.bokatuk@dreamteam-mobile.com>
2020-07-14 00:01:37 +0300

    Update JsLogFlush.php

--- JsLogFlush.php
+++ JsLogFlush.php
@@ -18,31 +18,89 @@ if (!('logflush' in window)) {
 
 window.logflush = {
     log: function(s) {
-		window.oldsole.log(s + ' ' + JSON.stringify(arguments));
         if (arguments.length <= 1) {
-            if (typeof s == 'undefined') flushEx();
-            else log(s);
-            return;
+            if (typeof s == 'undefined')
+			{
+				flushEx();
+			}
+            else if (typeof s == 'object')
+			{
+				window.oldsole.log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+				log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+			}
+			else
+			{
+				window.oldsole.log(s + ' ' + JSON.stringify(arguments));
+				log(s + ' ' + JSON.stringify(arguments));
+			}
         }
-        log(s + ' ' + JSON.stringify(arguments));
+		else if (typeof s == 'object')
+			{
+				window.oldsole.log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+				log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+			}
+			else
+			{
+				window.oldsole.log(s + ' ' + JSON.stringify(arguments));
+				log(s + ' ' + JSON.stringify(arguments));
+			}
     },
 	warn: function(s) {
-		window.oldsole.warn(s + ' ' + JSON.stringify(arguments));
-        if (arguments.length <= 1) {
-            if (typeof s == 'undefined') flushEx();
-            else log(s);
-            return;
+		if (arguments.length <= 1) {
+            if (typeof s == 'undefined')
+			{
+				flushEx();
+			}
+            else if (typeof s == 'object')
+			{
+				window.oldsole.warn(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+				log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+			}
+			else
+			{
+				window.oldsole.warn(s + ' ' + JSON.stringify(arguments));
+				log(s + ' ' + JSON.stringify(arguments));
+			}
         }
-        log(s + ' ' + JSON.stringify(arguments));
+		else if (typeof s == 'object')
+			{
+				window.oldsole.warn(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+				log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+			}
+			else
+			{
+				window.oldsole.warn(s + ' ' + JSON.stringify(arguments));
+				log(s + ' ' + JSON.stringify(arguments));
+			}
     },
 	error: function(s) {
-		window.oldsole.error(s + ' ' + JSON.stringify(arguments));
+		
         if (arguments.length <= 1) {
-            if (typeof s == 'undefined') flushEx();
-            else log(s);
-            return;
+            if (typeof s == 'undefined')
+			{
+				flushEx();
+			}
+            else if (typeof s == 'object')
+			{
+				window.oldsole.error(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+				log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+			}
+			else
+			{
+				window.oldsole.error(s + ' ' + JSON.stringify(arguments));
+				log(s + ' ' + JSON.stringify(arguments));
+			}
         }
-        log(s + ' ' + JSON.stringify(arguments));
+		else if (typeof s == 'object')
+			{
+				window.oldsole.error(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+				log(JSON.stringify(s) + ' ' + JSON.stringify(arguments));
+			}
+			else
+			{
+				window.oldsole.error(s + ' ' + JSON.stringify(arguments));
+				log(s + ' ' + JSON.stringify(arguments));
+			}
     }
 };
 <%SUBST_CONSOLE%>
